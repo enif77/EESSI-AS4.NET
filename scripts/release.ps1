ec61598ba19572ae35613e7a3ae9a0b3f65919ac
Přemysl Fára <premysl.fara@ezpada.com>
2022-02-16 09:50:55 +0100

    Fixed build script.

--- scripts/release.ps1
+++ scripts/release.ps1
@@ -17,8 +17,8 @@ $env:BUILD_BUILDNUMBER = '4.0.1.1000'
 # Python 2.7 is required for 'npm install'.
 #SET PYTHON=C:\Devel\bin\Python27
 #SET Path=C:\Devel\bin\Python27;C:\Devel\bin\Python27\Scripts;%PATH%
-$env:PYTHON = C:\Devel\bin\Python27
-$env:Path = C:\Devel\bin\Python27;C:\Devel\bin\Python27\Scripts;%PATH%
+$env:PYTHON = 'C:\Devel\bin\Python27'
+$env:Path = 'C:\Devel\bin\Python27;C:\Devel\bin\Python27\Scripts;%PATH%'
 
 Set-Location "./source/Eu.EDelivery.AS4.FE/ui"
 npm install node-sass
