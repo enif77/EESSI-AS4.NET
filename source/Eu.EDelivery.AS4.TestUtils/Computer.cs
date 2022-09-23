127849c1c259a98efe9ec28ab1ac21c18b56d503
Premysl Fara <premysl.fara@gmail.com>
2022-09-23 15:05:41 +0200

    Test projects dependencies cleanup.

--- source/Eu.EDelivery.AS4.TestUtils/Computer.cs
+++ source/Eu.EDelivery.AS4.TestUtils/Computer.cs
@@ -1,16 +1,16 @@
-﻿using System.Management.Automation;
+﻿//using System.Management.Automation;
 
-namespace Eu.EDelivery.AS4.TestUtils
-{
-    public static class Computer
-    {
-        public static void RunCommand(string command)
-        {
-            using (PowerShell instance = PowerShell.Create())
-            {
-                instance.AddScript(command);
-                instance.Invoke();
-            }
-        }
-    }
-}
+//namespace Eu.EDelivery.AS4.TestUtils
+//{
+//    public static class Computer
+//    {
+//        public static void RunCommand(string command)
+//        {
+//            using (PowerShell instance = PowerShell.Create())
+//            {
+//                instance.AddScript(command);
+//                instance.Invoke();
+//            }
+//        }
+//    }
+//}
